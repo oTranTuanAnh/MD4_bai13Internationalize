c452ddaf27fdd665a9571f8456c1154636922c9b
tuan anh <tuananhtran.xd@gmail.com>
2024-03-13 14:19:23 +0700

    first commit

---
+++ src/main/java/com/demo/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.demo.controller;
+
+
+import com.demo.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LoginController {
+
+
+}
